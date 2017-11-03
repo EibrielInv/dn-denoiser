6eb591a036e7d9fefc97b96b242ce7e070113b62
Eibriel <eibriel@yahoo.com.ar>
2017-11-02 21:13:43 -0300

    Processing EXR images

--- denoiser.py
+++ denoiser.py
@@ -9,7 +9,7 @@ import time
 import Imath
 import OpenEXR
 
-def load_exr(image_path, channels):
+def load_exr(image_path, channels, mergecol=False):
     # Open the input file
     file = OpenEXR.InputFile(image_path)
     # Compute the size
@@ -20,10 +20,51 @@ def load_exr(image_path, channels):
     # Read the channels as 32-bit floats
     FLOAT = Imath.PixelType(Imath.PixelType.FLOAT)
     image_data = []
+    add_channels = ["RenderLayer.DiffCol.R",
+                    "RenderLayer.DiffCol.G",
+                    "RenderLayer.DiffCol.B",
+                    "RenderLayer.SubsurfaceCol.R",
+                    "RenderLayer.SubsurfaceCol.G",
+                    "RenderLayer.SubsurfaceCol.B",
+                    "RenderLayer.GlossCol.R",
+                    "RenderLayer.GlossCol.G",
+                    "RenderLayer.GlossCol.B",
+                    "RenderLayer.TransCol.R",
+                    "RenderLayer.TransCol.G",
+                    "RenderLayer.TransCol.B",
+                    "RenderLayer.Env.R",
+                    "RenderLayer.Env.G",
+                    "RenderLayer.Env.B"]
+    color_channel_r = np.zeros([sz_x*sz_y], dtype = np.float32)
+    color_channel_g = np.zeros([sz_x*sz_y], dtype = np.float32)
+    color_channel_b = np.zeros([sz_x*sz_y], dtype = np.float32)
     for channel_name in channels:
-        image_data.append(np.fromstring( file.channel(channel_name, FLOAT), dtype = np.float32))
+        numpy_array = np.fromstring( file.channel(channel_name, FLOAT), dtype = np.float32)
+        if channel_name == "RenderLayer.Depth.Z":
+            numpy_array[numpy_array==10000000000] = 0
+            numpy_array = numpy_array + 0.000001
+            numpy_array= (numpy_array / (numpy_array.max()*0.5)) - 1.0
+        if channel_name in add_channels and mergecol:
+            if channel_name.endswith(".R"):
+                color_channel_r = color_channel_r + numpy_array
+            elif channel_name.endswith(".G"):
+                color_channel_g = color_channel_g + numpy_array
+            elif channel_name.endswith(".B"):
+                color_channel_b = color_channel_b + numpy_array
+            continue
+        if not channel_name.startswith("RenderLayer.Normal"):
+            if mergecol:
+                numpy_array[numpy_array>1.0] = 1.0
+                numpy_array = (numpy_array*2.0)-1.0
+        image_data.append(numpy_array)
+    channels_len = len(channels)
+    if mergecol:
+        image_data.append(color_channel_r)
+        image_data.append(color_channel_g)
+        image_data.append(color_channel_b)
+        channels_len = channels_len-15+3
     image_data = np.array(image_data)
-    image_data = image_data.reshape(len(channels), sz_y, sz_x)
+    image_data = image_data.reshape(channels_len, sz_y, sz_x)
     image_data = image_data.T
     image_data = np.rot90(image_data, -1)
     return image_data, file.header()
@@ -45,7 +86,7 @@ def save_exr(image_path, channel_names, cannels_data, header):
 # Should be 55
 blur_size = 9
 
-def create_blur_matrix (blur_size):
+def create_blur_matrix (blur_size, channel_count):
     # 1 2 3
     # 4 5 6
     # 7 8 9
@@ -63,48 +104,65 @@ def create_blur_matrix (blur_size):
     # 7 0 0    8 0 0    9 0 0
     # 0 7 0    0 8 0    0 9 0
     # 0 0 7    0 0 8    0 0 9
-    entity_matrix = np.zeros((blur_size, blur_size, 3, 3))
+    entity_matrix = np.zeros((blur_size, blur_size, channel_count, channel_count))
     for n in range(blur_size):
         for nn in range(blur_size):
-            for channels in range(3):
+            for channels in range(channel_count):
                 entity_matrix[n][nn][channels][channels] = 1.0
 
-    blur_matrix = tf.Variable(tf.ones([blur_size * blur_size]),
+    blur_matrix = tf.Variable(tf.random_uniform([blur_size * blur_size], 0.0, 1.0),
                                 name="blur_matrix")
     blur_matrix = tf.nn.softmax(blur_matrix)
     blur_matrix = tf.reshape(blur_matrix, [blur_size, blur_size])
     blur_matrix_img = tf.reshape(blur_matrix, [blur_size, blur_size, 1])
     tf.summary.image('blur_matrix', [blur_matrix_img])
 
-    blur_matrix = tf.stack([blur_matrix, blur_matrix, blur_matrix], axis=2)
-    blur_matrix = tf.stack([blur_matrix, blur_matrix, blur_matrix], axis=2)
+    stack = []
+    for n in range(channel_count):
+        stack.append(blur_matrix)
+    blur_matrix = tf.stack(stack, axis=2)
+    stack = []
+    for n in range(channel_count):
+        stack.append(blur_matrix)
+    blur_matrix = tf.stack(stack, axis=2)
     blur_matrix = blur_matrix * entity_matrix # Masks transformations per channel
     return blur_matrix
 
 
 def get_blur_parameters(image):
     #
-    kernel_size = 9
+    kernel_size = 18
     stride_size = 1
-    outputs = 10
-    #
+    outputs = 27
     weight = tf.Variable(tf.random_normal([kernel_size,
                                            kernel_size,
                                            image.shape[2],
                                            outputs], mean=0, stddev=0.08))
     bias = tf.Variable(tf.random_normal([outputs], mean=0, stddev=0.08))
-
     batch = tf.stack([image], name="Packed")
     x_tensor = tf.nn.conv2d(batch, weight, strides=[1, stride_size, stride_size, 1], padding='SAME')
     x_tensor = tf.nn.bias_add(x_tensor, bias)
     x_tensor = tf.nn.relu6(x_tensor)
     #
+    if 1:
+        kernel_size = 9
+        stride_size = 1
+        outputs = 54
+        weight = tf.Variable(tf.random_normal([kernel_size,
+                                               kernel_size,
+                                               27,
+                                               outputs], mean=0, stddev=0.08))
+        bias = tf.Variable(tf.random_normal([outputs], mean=0, stddev=0.08))
+        x_tensor = tf.nn.conv2d(x_tensor, weight, strides=[1, stride_size, stride_size, 1], padding='SAME')
+        x_tensor = tf.nn.bias_add(x_tensor, bias)
+        x_tensor = tf.nn.relu6(x_tensor)
+    #
     kernel_size = 1
     stride_size = 1
-    outputs = 3
+    outputs = 6
     weight = tf.Variable(tf.random_normal([kernel_size,
                                            kernel_size,
-                                           10,
+                                           54,
                                            outputs], mean=0, stddev=0.08))
     bias = tf.Variable(tf.random_normal([outputs], mean=0, stddev=0.08))
 
@@ -150,43 +208,157 @@ print ("Load images")
 # Texture values for second intersection (r g b)
 # Direct illumination visibility (0 1)
 
-channels = ["RenderLayer.Combined.R", "RenderLayer.Combined.G", "RenderLayer.Combined.B"]
+# SubsurfaceDir.B
+# RenderLayer.GlossCol.R
+# RenderLayer.Emit.B
+# RenderLayer.GlossInd.G
+# RenderLayer.SubsurfaceDir.G
+# RenderLayer.GlossDir.R
+# RenderLayer.TransInd.R
+# RenderLayer.GlossDir.G
+# RenderLayer.SubsurfaceInd.R
+# RenderLayer.SubsurfaceInd.G
+# RenderLayer.SubsurfaceInd.B
+# RenderLayer.GlossInd.B
+# RenderLayer.Shadow.B
+# RenderLayer.GlossInd.R
+# RenderLayer.SubsurfaceCol.B
+# RenderLayer.TransInd.G
+# RenderLayer.GlossCol.B
+# RenderLayer.SubsurfaceCol.R
+# RenderLayer.SubsurfaceCol.G
+# RenderLayer.TransInd.B
+# RenderLayer.TransCol.R
+# RenderLayer.Normal.X
+# RenderLayer.Normal.Y
+# RenderLayer.Normal.Z
+# RenderLayer.SubsurfaceDir.R
+# RenderLayer.DiffCol.G
+# RenderLayer.DiffCol.R
+# RenderLayer.Combined.B
+# RenderLayer.DiffDir.R
+# RenderLayer.DiffDir.G
+# RenderLayer.DiffDir.B
+# RenderLayer.Combined.G
+# RenderLayer.Env.G
+# RenderLayer.GlossDir.B
+# RenderLayer.Combined.R
+# RenderLayer.TransDir.G
+# RenderLayer.Shadow.R
+# RenderLayer.DiffInd.B
+# RenderLayer.TransCol.G
+# RenderLayer.DiffInd.R
+# RenderLayer.GlossCol.G
+# RenderLayer.Env.B
+# RenderLayer.Emit.R
+# RenderLayer.Emit.G
+# RenderLayer.TransCol.B
+# RenderLayer.DiffInd.G
+# RenderLayer.DiffCol.B
+# RenderLayer.Env.R
+# RenderLayer.TransDir.B
+# RenderLayer.Combined.A
+# RenderLayer.TransDir.R
+# RenderLayer.Shadow.G
+# RenderLayer.Depth.Z
 
-original_image, original_header = load_exr("train_images/box_low.exr", channels)
-target_image, _ = load_exr("train_images/box_high.exr", channels)
+#low_image = "train_images/box_low.exr"
+#high_image = "train_images/box_high.exr"
+low_image = "train_images/agent_low.exr"
+high_image = "train_images/agent_low.exr"
+
+channels = ["RenderLayer.Combined.R",
+            "RenderLayer.Combined.G",
+            "RenderLayer.Combined.B",
+            "RenderLayer.DiffCol.R",
+            "RenderLayer.DiffCol.G",
+            "RenderLayer.DiffCol.B",
+            "RenderLayer.SubsurfaceCol.R",
+            "RenderLayer.SubsurfaceCol.G",
+            "RenderLayer.SubsurfaceCol.B",
+            "RenderLayer.GlossCol.R",
+            "RenderLayer.GlossCol.G",
+            "RenderLayer.GlossCol.B",
+            "RenderLayer.TransCol.R",
+            "RenderLayer.TransCol.G",
+            "RenderLayer.TransCol.B",
+            "RenderLayer.Normal.X",
+            "RenderLayer.Normal.Y",
+            "RenderLayer.Normal.Z",
+            "RenderLayer.Depth.Z",
+            "RenderLayer.Env.R",
+            "RenderLayer.Env.G",
+            "RenderLayer.Env.B"]
+features_image, original_header = load_exr(low_image, channels, mergecol=True)
+channels = ["RenderLayer.DiffDir.R",
+            "RenderLayer.DiffDir.G",
+            "RenderLayer.DiffDir.B",
+            "RenderLayer.DiffInd.R",
+            "RenderLayer.DiffInd.G",
+            "RenderLayer.DiffInd.B",
+            "RenderLayer.SubsurfaceDir.R",
+            "RenderLayer.SubsurfaceDir.G",
+            "RenderLayer.SubsurfaceDir.B",
+            "RenderLayer.SubsurfaceInd.R",
+            "RenderLayer.SubsurfaceInd.G",
+            "RenderLayer.SubsurfaceInd.B",
+            "RenderLayer.GlossDir.R",
+            "RenderLayer.GlossDir.G",
+            "RenderLayer.GlossDir.B",
+            "RenderLayer.GlossInd.R",
+            "RenderLayer.GlossInd.G",
+            "RenderLayer.GlossInd.B",
+            "RenderLayer.TransDir.R",
+            "RenderLayer.TransDir.G",
+            "RenderLayer.TransDir.B",
+            "RenderLayer.TransInd.R",
+            "RenderLayer.TransInd.G",
+            "RenderLayer.TransInd.B"]
+original_image, _ = load_exr(low_image, channels)
+target_image, _ = load_exr(high_image, channels)
+
+features_image_place = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], image_shape[2]], name="x")
+original_image_place = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], image_shape[2]], name="xb")
+target_image_place = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], image_shape[2]], name="y")
+
 
 print ("Create graph")
 with tf.name_scope('blur_parameters'):
+    # Run get_blur_parameters for:
+    # DiffDir
+    # DiffInd
+    # SubsurfaceDir
+    # SubsurfaceInd
+    # GlossDir
+    # GlossInd
+    # TransDir
+    # TransInd
+
+    blur_filters_count = 6
+
     # Analyze image
-    blur_parameters = get_blur_parameters(original_image)
+    blur_parameters = get_blur_parameters(features_image)
     tf.summary.histogram('blur_parameters', blur_parameters)
     blur_parameters = tf.transpose(blur_parameters[0])
-    blur_parameters_a = tf.transpose(blur_parameters[0])
-    blur_parameters_a = tf.stack([blur_parameters_a, blur_parameters_a, blur_parameters_a], axis=2)
-    tf.summary.image('blur_parameters_images_a', [blur_parameters_a])
-    blur_parameters_b = tf.transpose(blur_parameters[1])
-    blur_parameters_b = tf.stack([blur_parameters_b, blur_parameters_b, blur_parameters_b], axis=2)
-    tf.summary.image('blur_parameters_images_b', [blur_parameters_b])
-    blur_parameters_c = tf.transpose(blur_parameters[2])
-    blur_parameters_c = tf.stack([blur_parameters_c, blur_parameters_c, blur_parameters_c], axis=2)
-    tf.summary.image('blur_parameters_images_c', [blur_parameters_c])
+    blur_parameters_array = []
+    for filter_id in range(blur_filters_count):
+        blur_parameters_array.append(tf.transpose(blur_parameters[filter_id]))
+        stack = []
+        for n in range(len(channels)):
+            stack.append(blur_parameters_array[-1])
+        blur_parameters_array[-1] = tf.stack(stack, axis=2)
+        tf.summary.image('blur_parameters_images_{}'.format(filter_id),
+                            [tf.reduce_sum(blur_parameters_array[-1], 2, keep_dims=True)])
 
 with tf.name_scope('apply_blur'):
-    # Blur image A
-    blur_matrix = create_blur_matrix(blur_size)
-    blured_image = apply_blur(original_image, blur_matrix)
-    tf.summary.image('blured_image', [blured_image])
-    # Blur image B
-    blur_matrix_b = create_blur_matrix(blur_size)
-    blured_image_b = apply_blur(original_image, blur_matrix_b)
-    tf.summary.image('blured_image_b', [blured_image_b])
-    # Blur image B
-    blur_matrix_c = create_blur_matrix(blur_size)
-    blured_image_c = apply_blur(original_image, blur_matrix_c)
-    tf.summary.image('blured_image_c', [blured_image_c])
-
-    final_image = (blured_image * blur_parameters_a) + (blured_image_b * blur_parameters_b) + (blured_image_c * blur_parameters_c)
-    tf.summary.image('final_image', [final_image])
+    blur_matrix_array = []
+    final_image = tf.zeros([original_image.shape[0], original_image.shape[1], original_image.shape[2]])
+    for filter_id in range(blur_filters_count):
+        # Blur image
+        blur_matrix_array.append(create_blur_matrix(blur_size, len(channels)))
+        blured_image = apply_blur(original_image, blur_matrix_array[-1])
+        tf.summary.image('blured_image_{}'.format(filter_id), [tf.reduce_sum(blured_image, 2, keep_dims=True)])
+        final_image += (blured_image * blur_parameters_array[filter_id])
 
 # Set cost
 with tf.name_scope('train'):
@@ -194,21 +366,36 @@ with tf.name_scope('train'):
     tf.summary.scalar('mse_cost', mse_cost)
     train_step = tf.train.AdamOptimizer().minimize(mse_cost)
 
+training = False
+
+saver = tf.train.Saver()
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
+    if not training or True:
+        # Restore variables from disk.
+        saver.restore(sess, tf.train.latest_checkpoint("/home/gabriel/dev/denoiser/dn-denoiser"))
+        print("Model restored.")
+    else:
+        sess.run(init_op)
     merged = tf.summary.merge_all()
     train_writer = tf.summary.FileWriter('train', sess.graph)
     print ("Run session")
-    sess.run(init_op)
 
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
 
-    for i in range(10000):
-        summary, cost, parameters, output_image, _ = sess.run([merged, mse_cost, blur_parameters_a, final_image, train_step])
-        if i % 10 == 0:
-            train_writer.add_summary(summary, i)
+    for step in range(10000):
+        if training:
+            summary, cost, parameters, output_image, _ = sess.run([merged, mse_cost, blur_parameters_array[0], final_image, train_step])
+        else:
+            summary, parameters, output_image, _ = sess.run([merged, blur_parameters_array[0], final_image, train_step])
+        if step % 10 == 0 or not training:
+            train_writer.add_summary(summary, step)
             save_exr("train_images/output.exr", channels, output_image, original_header)
+            if training:
+                saver.save(sess, 'denoiser-model', global_step=step)
+        if not training:
+            break
 
     coord.request_stop()
     coord.join(threads)
